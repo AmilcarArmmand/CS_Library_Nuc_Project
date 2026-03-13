400fa6fd07a25b3ce7d50dd222e502ef0b0deb4b
Amilcar Armmand <armmand@gmail.com>
2026-03-13 02:50:14 +0000

    feat: updated .gitignore and added PM2 config

---
+++ src/ecosystem.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [{
+    name: 'CS Library',
+    script: 'app.js',
+    instances: 1,
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true,
+    max_memory_restart: '1G',
+    restart_delay: 4000
+  }]
+};
